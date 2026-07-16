32195a521666fafd0e4962285e72438d0f9cc8bf
IonUdvas <udv.ion8679@gmail.com>
2026-07-16 20:03:41 +0530

    Created Dataset and DataLoader

--- input_creation/auction_dataset_utils.py
+++ input_creation/auction_dataset_utils.py
@@ -4,6 +4,101 @@ from input_creation.auction_state.auction_state import AuctionReplayEngine
 from input_creation.auction_state.utils import build_bid_summary
 from .auction_state.utils import build_bid_summary
 
+class LabelEncoder:
+
+    def __init__(self):
+
+        self.label_to_idx = {}
+        self.idx_to_label = {}
+
+    def fit(self, values):
+
+        values = (
+            pd.Series(values)
+            .dropna()
+            .unique()
+        )
+
+        values = sorted(values)
+
+        self.label_to_idx = {
+            label: idx
+            for idx, label in enumerate(values)
+        }
+
+        self.idx_to_label = {
+            idx: label
+            for label, idx in self.label_to_idx.items()
+        }
+
+        return self
+
+    def transform(self, values):
+
+        return (
+            pd.Series(values)
+            .map(self.label_to_idx)
+            .astype(int)
+        )
+
+    def fit_transform(self, values):
+
+        self.fit(values)
+
+        return self.transform(values)
+
+    def inverse_transform(self, values):
+
+        return (
+            pd.Series(values)
+            .map(self.idx_to_label)
+        )
+
+    @property
+    def classes_(self):
+
+        return list(self.label_to_idx.keys())
+    
+class EncoderManager:
+
+    def __init__(self):
+
+        self.encoders = {}
+
+    def fit(self, df, columns):
+
+        for column in columns:
+
+            encoder = LabelEncoder()
+
+            encoder.fit(df[column])
+
+            self.encoders[column] = encoder
+
+        return self
+
+    def transform(self, df):
+
+        df = df.copy()
+
+        for column, encoder in self.encoders.items():
+
+            df[column] = encoder.transform(df[column])
+
+        return df
+
+    def fit_transform(self, df, columns):
+
+        self.fit(df, columns)
+
+        return self.transform(df)
+
+    def get_encoder(self, column):
+
+        return self.encoders[column]
+    
+
+
 def build_training_samples(
     player_df_PATH,
     bid_df_PATH,
@@ -123,3 +218,17 @@ def build_training_samples(
     ]
 
     return training_df
+
+def build_encoders(training_df):
+
+    manager = EncoderManager()
+
+    manager.fit(
+        training_df,
+        [
+            "team",
+            "role"
+        ]
+    )
+
+    return manager
